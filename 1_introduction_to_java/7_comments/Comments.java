ca8cd0c14fc870d6370357ec79157df7ae25f18e
Craig Baird <bairdcraig@gmail.com>
2017-06-27 18:01:06 -0500

    some progress

---
+++ 1_introduction_to_java/7_comments/Comments.java
@@ -0,0 +1,13 @@
+public class Comments {
+	public static void main(String[] args) {
+
+//		System.out.println("Noise!");
+
+/*
+This is a
+multiple line
+comment
+*/
+
+	}
+}
---
+++ 1_introduction_to_java/8_math/Arithmetic.java
@@ -0,0 +1,8 @@
+public class Arithmetic {
+	public static void main(String[] args) {
+
+		int myNumber = 8 * 9;
+		System.out.println(myNumber);
+
+	}
+}
